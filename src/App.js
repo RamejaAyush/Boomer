50a25c0196a918864ef1571486a72ddee48be113
Ayush Rameja <67544555+RamejaAyush@users.noreply.github.com>
2021-09-20 11:51:05 +0530

    added svgs in the background

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import Nav from "./components/Nav";
 import "./styles/global.scss";
+import Bubble from "./Media/bubble.svg";
 
 const App = () => {
  return (
   <>
+   <div className="background-bubble">
+    <img src={Bubble} className="left bubble" alt="" />
+    <img src={Bubble} className="right bubble" alt="" />
+   </div>
    <Nav />
   </>
  );
